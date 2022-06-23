f6e6b967654446e46cfd0c74603a9fb0ed636fab
Samuel Martins <101484995+Samuel-GMartins@users.noreply.github.com>
2022-06-22 21:28:09 -0300

    att inicial"

--- http/server.js
+++ http/server.js
@@ -1,10 +1,28 @@
-const { read } = require('fs')
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 const door = 5000
 
-// res = Requisição
+
+
+// req = Requisição
 // res = Resposta
-http.createServer((req,res) =>{
-    res.write("funfando Legal")
-    res.end()
+http.createServer((req, res) => {
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname,'public',file)
+
+    res.end('cheguei')
+    //     if(req.url === '/')
+    fs.readFile(
+        path.join(__dirname, 'public',file),
+        (err, content) => {
+            if (err) throw err
+
+            res.end(content)
+        }
+    )
+
+    //     if(req.url === '/contato')
+    //     return res.end('<h1> Contato<h1>')
+
 }).listen(door, () => console.log(`Server is running in the door ${door}`))
