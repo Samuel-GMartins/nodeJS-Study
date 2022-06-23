8bb87863a317e789d6a2a95d1f502148a83b7d68
Samuel Martins <101484995+Samuel-GMartins@users.noreply.github.com>
2022-06-22 21:39:58 -0300

    att inicial"

--- http/server.js
+++ http/server.js
@@ -8,13 +8,18 @@ const door = 5000
 // req = Requisição
 // res = Resposta
 http.createServer((req, res) => {
+
     const file = req.url === '/' ? 'index.html' : req.url
     const filePath = path.join(__dirname,'public',file)
+    const extname = path.extname(filePath)
 
-    res.end('cheguei')
-    //     if(req.url === '/')
+    const allowedFileTypes = ['.html','.css','.js']
+    const allowed = allowedFileTypes.find(item => item == extname)
+
+    if (!allowed) return
+
     fs.readFile(
-        path.join(__dirname, 'public',file),
+        filePath,
         (err, content) => {
             if (err) throw err
 
