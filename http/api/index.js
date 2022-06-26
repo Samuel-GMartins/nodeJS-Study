029806fde112bc5742c60303670ebb860159bb9a
Samuel Martins <101484995+Samuel-GMartins@users.noreply.github.com>
2022-06-26 14:37:57 -0300

    projeto ifinal

--- http/api/index.js
+++ http/api/index.js
@@ -3,42 +3,38 @@ const URL = require('url')
 const fs = require('fs')
 const path = require('path')
 
-const door = 3000
-
-
 const data = require('./urls.json')
 
 function writeFile(cb) {
     fs.writeFile(
-        path.join(__dirname, "urls.json"),
+        path.join(__dirname, "urls.json"), 
         JSON.stringify(data, null, 2),
         err => {
-            if (err) throw err
-            
-            cb(JSON.stringify({message: "OK"}))
+            if(err) throw err
+
+            cb(JSON.stringify({message: "ok"}))
         }
     )
 }
 
 http.createServer((req, res) => {
-    const {name,url,del} = URL.parse(req.url, true).query
+    const { name, url, del } = URL.parse(req.url, true).query
 
-    res.writeHead(200,{
-        'Acess-Control-Allow-Origin': '*'
+    res.writeHead(200, {
+        'Access-Control-Allow-Origin': '*'
     })
-    //all Resources
-    if (!name || !url)
+
+    // all resources
+    if(!name || !url)
         return res.end(JSON.stringify(data))
 
-    if (del) {
+    if(del) {
         data.urls = data.urls.filter(item => String(item.url) !== String(url))
-
-        return writeFile((message) => {res.end(message)})
-
+        return writeFile((message) => res.end(message))   
     }
 
-    data.urls.push({name,url})
+    data.urls.push({name, url})
 
-    return writeFile((message) => {res.end(message)})
+    return writeFile((message) => res.end(message)) 
 
-}).listen(door, () => console.log(`api is running in door ${door}`))
+}).listen(3000, () => console.log('Api is running'))
--- http/server.js
+++ http/server.js
@@ -1,33 +1,25 @@
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
-const door = 5000
 
 
-
-// req = Requisição
-// res = Resposta
 http.createServer((req, res) => {
 
     const file = req.url === '/' ? 'index.html' : req.url
-    const filePath = path.join(__dirname,'public',file)
+    const filePath = path.join(__dirname, 'public', file)
     const extname = path.extname(filePath)
 
-    const allowedFileTypes = ['.html','.css','.js']
+    const allowedFileTypes = ['.html', '.css', '.js']
     const allowed = allowedFileTypes.find(item => item == extname)
 
-    if (!allowed) return
+    if(!allowed) return
 
     fs.readFile(
         filePath,
         (err, content) => {
-            if (err) throw err
+            if(err) throw err
 
             res.end(content)
         }
     )
-
-    //     if(req.url === '/contato')
-    //     return res.end('<h1> Contato<h1>')
-
-}).listen(door, () => console.log(`Server is running in the door ${door}`))
+}).listen(5000, () => console.log('Server is running'))
